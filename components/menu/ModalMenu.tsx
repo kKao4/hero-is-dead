fd9a3043a87f4020c146dc6c87d13de4cbbdda3c
kKao2 <thanh291100@gmail.com>
2023-12-20 18:16:26 +0700

    fix modal menu active link

--- components/menu/ModalMenu.tsx
+++ components/menu/ModalMenu.tsx
@@ -6,6 +6,7 @@ import { useDispatch, useSelector } from "react-redux"
 import { globalSelector, toggleOpenModalMenu } from "@/redux/globalSlice"
 import { twJoin } from "tailwind-merge"
 import { CSSTransition } from 'react-transition-group';
+import { pages } from "@/data"
 
 export default function ModalMenu() {
   const globalState = useSelector(globalSelector)
@@ -18,23 +19,33 @@ export default function ModalMenu() {
       unmountOnExit
     >
       <div
-        className={twJoin(
-          "fixed top-0 w-full ease-out py-18 bg-root-white z-40",
-        )}
+        className="fixed top-0 w-full ease-out py-14 bg-root-white z-40"
         style={{ height: "100dvh" }}
       >
         <Link href="/" onClick={() => dispatch(toggleOpenModalMenu())}>
           <Image src={logo_mobile} alt="" className="block mx-auto max-h-14 max-w-fit lg:hidden" quality={50} />
         </Link>
-        <div className="flex flex-col py-10 gap-y-6">
-          <MenuLink href="/news/page/1" title_1="News" title_2="Tin Tức" mobile={true} />
+        <div className="flex flex-col py-8 gap-y-6">
+          {/* <MenuLink href="/news/page/1" title_1="News" title_2="Tin Tức" mobile={true} />
           <MenuLink href="/on-air" title_1="On Air" title_2="Phát Sóng" mobile={true} />
           <MenuLink href="/story" title_1="Story" title_2="Cốt Truyện" mobile={true} />
           <MenuLink href="/staff-and-cast" title_1="STAFF&CAST" title_2="Nhân viên và diễn viên" mobile={true} />
           <MenuLink href="/character" title_1="CHARACTER" title_2="Nhân Vật" mobile={true} />
           <MenuLink href="/product" title_1="PRODUCT" title_2="Sản Phẩm" mobile={true} />
           <MenuLink href="/special" title_1="SPECIAL" title_2="Đặc Biệt" mobile={true} />
-          <MenuLink url="https://twitter.com/yuusyagasinda" title_1="TWITTER" title_2="TWITTER" mobile={true} />
+          <MenuLink url="https://twitter.com/yuusyagasinda" title_1="TWITTER" title_2="TWITTER" mobile={true} /> */}
+          {pages.map(page => {
+            return (
+              <MenuLink
+                key={page.href}
+                mobile href={page.href}
+                url={page.url}
+                title_1={page.title_1}
+                title_2={page.title_2}
+                isActive={page.href ? page.href.includes(globalState.activePage) : false}
+              />
+            )
+          })}
         </div>
       </div>
     </CSSTransition>
